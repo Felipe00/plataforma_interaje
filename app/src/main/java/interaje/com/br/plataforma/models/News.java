bc149b337b2d080ff5de154dfa3c2b4b4d5e3e14
Felipe Costa <fcostax02@gmail.com>
2016-07-12 17:09:17 -0300

    Estrutura base

---
+++ app/src/main/java/interaje/com/br/plataforma/models/News.java
@@ -0,0 +1,13 @@
+package interaje.com.br.plataforma.models;
+
+/**
+ * Created by rayquaza on 12/07/16.
+ */
+// Notícias
+public class News {
+
+    private Long id;
+    private String title;
+    private String message;
+
+}
---
+++ app/src/main/java/interaje/com/br/plataforma/models/Teatcher.java
@@ -0,0 +1,12 @@
+package interaje.com.br.plataforma.models;
+
+/**
+ * Created by rayquaza on 12/07/16.
+ */
+// Professor
+public class Teatcher extends User {
+
+    public Teatcher() {
+        setRole(Role.TEATCHER);
+    }
+}
