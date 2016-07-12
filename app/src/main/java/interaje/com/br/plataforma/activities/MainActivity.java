8b96de30c69b75ea635237fd1ee5e1c78e4dec70
Felipe Costa <fcostax02@gmail.com>
2016-07-12 17:09:17 -0300

    Alterações para aula de quarta-feira

---
+++ app/src/main/java/interaje/com/br/plataforma/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package interaje.com.br.plataforma.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import interaje.com.br.plataforma.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/interaje/com/br/plataforma/models/Teatcher.java
@@ -0,0 +1,14 @@
+package interaje.com.br.plataforma.models;
+
+/**
+ * Created by rayquaza on 12/07/16.
+ */
+// Professor
+public class Teatcher extends User {
+
+    private final Role role;
+
+    public Teatcher() {
+        this.role = Role.TEATCHER;
+    }
+}
